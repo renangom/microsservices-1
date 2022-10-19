539f18b69887b6913f32f5467e554660565f6ec5
Renan <renan23eduardo@gmail.com>
2022-10-19 10:23:30 -0300

    Create event sending in comments service

--- comments/index.js
+++ comments/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {randomBytes} = require('crypto');
-const cors = require('cors')
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
@@ -21,6 +22,15 @@ app.post('/post/:id/comments', (req, res) => {
     comments.push({id: commentId, content: content});
 
     commentsByPostId[postId] = comments;
+
+    axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId
+        }
+    })
     res.status(201).send(comments);
 })
 
