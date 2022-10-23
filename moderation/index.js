0f03f8bcc0d3ad24ee61828f66a200a85f8f7b9e
Renan <renan23eduardo@gmail.com>
2022-10-23 14:31:07 -0300

    Create moderation

---
+++ moderation/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+
+
+
+app.post('/events', (req,res) => {
+    
+})
+
+
+app.listen(4003, () => {
+    console.log("App is running on port 4003")
+})
