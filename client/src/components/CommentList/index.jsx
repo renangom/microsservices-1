37ebf9dcd01a0f496e1ba3199744d309a5428805
Renan <renan23eduardo@gmail.com>
2022-10-24 09:20:42 -0300

    Filter comments in client side

--- client/src/components/CommentList/index.jsx
+++ client/src/components/CommentList/index.jsx
@@ -3,11 +3,23 @@ import './style.css'
 
 
 export default function CommentList({comments}) {
+  
   return (
     <ul className='commentList'>
         {comments.map((comment) => {
+          let content;
+
+          if(comment.status === 'aproved'){
+            content = comment.content
+          }
+          if(comment.status === 'pending'){
+            content = "This comment is awaiting moderation"
+          }
+          if(comment.status === 'rejected'){
+            content = 'This comment has been rejected'
+          }
             return(
-                <li className='contentList' key={comment.id}>{comment.content}</li>
+                <li className='contentList' key={comment.id}>{content}</li>
             )
         })}
     </ul>
